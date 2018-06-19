98ac0dcc6f02f8a328e0fab844d384038b58a867
comwrg <xcomwrg@gmail.com>
2018-06-19 14:54:43 +0800

    add exit alert dialog on the send email activity

--- app/src/main/java/me/demo/email/activity/SendEmailActivity.java
+++ app/src/main/java/me/demo/email/activity/SendEmailActivity.java
@@ -1,5 +1,7 @@
 package me.demo.email.activity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -46,26 +48,55 @@ public class SendEmailActivity extends BaseActivity {
     }
 
     @Override
-    protected void onPause() {
-        super.onPause();
+    public void onBackPressed() {
         if (id == -2)
             return;
-        String receiver = txt_receiver.getText().toString();
-        String title = txt_title.getText().toString();
-        String content = txt_content.getText().toString();
-        boolean r = false;
-        if (id > -1) {
-            r = email.updateDrafts(id, title, content, receiver);
-        } else {
-            r = email.addDrafts(title, content, receiver);
-        }
-        String s;
-        if (r) {
-            s = "邮件已存为草稿。";
-        } else {
-            s = "存草稿失败！";
-        }
-        Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setIcon(R.drawable.doraemon);
+        dialog.setTitle("提示").setMessage("是否保存为草稿？");
+
+        dialog.setNeutralButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+
+        dialog.setNegativeButton("不保存", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                SendEmailActivity.super.onBackPressed();
+            }
+        });
+
+        dialog.setPositiveButton("保存", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String receiver = txt_receiver.getText().toString();
+                String title = txt_title.getText().toString();
+                String content = txt_content.getText().toString();
+                boolean r = false;
+                if (id > -1) {
+                    r = email.updateDrafts(id, title, content, receiver);
+                } else {
+                    r = email.addDrafts(title, content, receiver);
+                }
+                String s;
+                if (r) {
+                    s = "邮件已存为草稿。";
+                } else {
+                    s = "存草稿失败！";
+                }
+                Toast.makeText(SendEmailActivity.this, s, Toast.LENGTH_SHORT).show();
+                SendEmailActivity.super.onBackPressed();
+            }
+        });
+
+        dialog.show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
     }
 
     @OnClick(R.id.fab)
